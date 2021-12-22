9887699dbd3dd6a703535916b459e03d3ab6758b
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-22 22:40:24 +0300

    restart workflow since it's going to stop soon

--- app/src/flarogus/util/ImageUtil.kt
+++ app/src/flarogus/util/ImageUtil.kt
@@ -5,20 +5,24 @@ import java.awt.image.*;
 object ImageUtil {
 	
 	/** Creates a new BufferedImage containing the result of multiplying the source image by the multiplier image */
-	fun multiply(source: BufferedImage, multiplier: BufferedImage): BufferedImage {
-		val new = BufferedImage(source.width, source.height, BufferedImage.TYPE_INT_ARGB);
+	fun multiply(target: BufferedImage, multiplier: BufferedImage): BufferedImage = operation(target, multiplier) { a, b -> a * b / 255 };
+	
+	/** Applies the providen operation to r, g and b components of each pixel of the target image
+	 *  The first lambda argument is the target component, the second is the additional component. */
+	inline fun operation(target: BufferedImage, additional: BufferedImage, op: (Int, Int) -> Int): BufferedImage {
+		val new = BufferedImage(target.width, target.height, BufferedImage.TYPE_INT_ARGB);
 		
-		val xRatio = multiplier.width.toDouble() / source.width;
-		val yRatio = multiplier.height.toDouble() / source.height
-		for (x in 0 until source.width) {
-			for (y in 0 until source.height) {
-				val sourcecol = source.getRGB(x, y)
-				val mulcol = multiplier.getRGB((x * xRatio).toInt(), (y * yRatio).toInt())
+		val xRatio = additional.width.toDouble() / target.width;
+		val yRatio = additional.height.toDouble() / target.height
+		for (x in 0 until target.width) {
+			for (y in 0 until target.height) {
+				val sourcecol = target.getRGB(x, y)
+				val mulcol = additional.getRGB((x * xRatio).toInt(), (y * yRatio).toInt())
 				
 				val a = if ((sourcecol shr 24) and 0xff < 10 || (mulcol shr 24) and 0xff < 10) 0 else 255
-				val r = ((sourcecol and 0x00ff0000) shr 16) * ((mulcol and 0x00ff0000) shr 16) / 255
-				val g = ((sourcecol and 0x0000ff00) shr 8) * ((mulcol and 0x0000ff00) shr 8) / 255
-				val b = (sourcecol and 0x000000ff) * (mulcol and 0x000000ff) / 255
+				val r = op(((sourcecol and 0x00ff0000) shr 16), ((mulcol and 0x00ff0000) shr 16))
+				val g = op(((sourcecol and 0x0000ff00) shr 8), ((mulcol and 0x0000ff00) shr 8))
+				val b = op((sourcecol and 0x000000ff), (mulcol and 0x000000ff))
 				
 				val newcol = ((a and 0xff) shl 24) or ((r and 0xff) shl 16) or ((g and 0xff) shl 8) or (b and 0xff)
 				new.setRGB(x, y, newcol)
