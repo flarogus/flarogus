663208008ebb94a1ca3de917c56fa08919e407cb
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-23 19:48:39 +0300

    I can't run it on my armhf machine, lets see if it'll run on an amd64 machine

--- app/src/flarogus/Flarogus.kt
+++ app/src/flarogus/Flarogus.kt
@@ -18,6 +18,9 @@ import flarogus.util.*;
 import flarogus.commands.*;
 
 suspend fun main(vararg args: String) = runBlocking {
+	//javax.script.ScriptEngineManager().engineFactories.forEach { println(it.extensions); println(it.engineName) }
+	//javax.script.ScriptEngineManager(Thread.currentThread().contextClassLoader).engineFactories.find { "kts" in it.extensions }!!.scriptEngine.eval("6 + 8")
+	
 	val token = args.getOrNull(0)
 	if (token == null) {
 		println("[ERROR] no token specified")
