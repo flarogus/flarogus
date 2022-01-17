7196b25905c37237ec326c25197322297cb1ebeb
mnemotechnician <daytimer253@gmail.com>
2022-01-17 13:00:26 +0300

    a

--- app/src/flarogus/commands/Commands.kt
+++ app/src/flarogus/commands/Commands.kt
@@ -196,7 +196,7 @@ fun initCommands() {
 		when (command) {
 			"listGuilds" -> {
 				val msg = Multiverse.multiverse.map {
-					message.supplier.getGuild(it.data.guildId)
+					message.supplier.getGuild(it.data.guildId.value)
 				}.toSet().map { "${it?.id?.value} - ${it?.name?.stripEveryone()}" }.joinToString(",\n")
 				replyWith(message, msg)
 			}
