928d50e73d30db8b9581cbe52c2ae5fb2845b70c
MHeMoTexHuK <daytimer253@gmail.com>
2022-04-28 21:19:22 +0300

    i forgot to do git add

---
+++ app/src/main/kotlin/command/impl/MinesweeperCommand.kt
@@ -0,0 +1,142 @@
+package flarogus.command.impl
+
+import kotlin.random.*;
+import kotlinx.coroutines.*;
+import dev.kord.core.entity.*;
+import dev.kord.core.event.message.*;
+import flarogus.util.*;
+import flarogus.command.*
+import flarogus.command.builder.*
+
+private val bombEmoji = "💥"
+private val numbers = arrayOf("0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣")
+
+private val maxW = 25
+private val maxH = 30
+
+private val openFlag = 0b10000
+
+fun TreeCommand.addMinesweeperSubcommand() = subcommand<Nothing?>("minesweeper") {
+	discordOnly()
+
+	description = "Play minesweeper in discord."
+
+	arguments {
+		default<Int>("width", "The width of the field. Defaults to 12. Maximum $maxW".) { 12 }
+		default<Int>("height", "The height of the field. Defaults to 8. Maximum $maxH.") { 18 }
+
+		default<Int>("mines", "The number of mines on the field. Defaults to (w * h / 3.3).") {
+			(args.arg<Int>("width") * args.arg<Int>("height") / 3.3f).toInt()
+		}
+	}
+
+	action {
+		val w = args.arg<Int>("width")
+		val h = args.arg<Int>("height")
+		val mines = args.arg<Int>("mines")
+
+		val mineLimit = w * h - 9
+
+		require(w > 3 && w <= maxW) { "Width must be in range of (3; $maxW]" }
+		require(h > 3 && h <= maxH) { "Height must be in range of (3; $maxH]" }
+		require(mines > 0 && mines < mineLimit) { "A ${w}x${h} field can not contain more than $mineLimit mines" }
+
+		val freeTiles = ArrayList<Int>(25 * 25) //each value represents both x and y component. assuming these components won't exceed 255.
+		val builder = StringBuilder(2000)
+		
+		//position that will be opened at the start
+		val openX = Random.nextInt(1, w - 1)
+		val openY = Random.nextInt(1, h - 1)
+		
+		//mine == -1, free == [0, 8] (number of mines), opened = [16, 24] (5th bit is active, other represent number of mines)
+		val field = Array(w) { IntArray(h) }
+
+		//first iteration: put all possible tiles in the list
+		freeTiles.clear()
+		for (x in 0 until w) {
+			for (y in 0 until h) {
+				if (Math.abs(x - openX) > 1 || Math.abs(y - openY) > 1) {
+					freeTiles += packTile(x, y)
+				}
+			}
+		}
+		//second iteration: shuffle the list and put bombs on the field at the first ${mines} positions taken from the list
+		freeTiles.shuffle()
+		for (i in 0 until Math.min(mines, freeTiles.size)) {
+			val tile = freeTiles.get(i)
+			val x = unpackX(tile)
+			val y = unpackY(tile)
+			field[x][y] = -1 //put a mine
+		}
+
+		//third iteration: for each tile, count neighbouring bombs, assign their number to the tile
+		for (x in 0 until w) {
+			for (y in 0 until h) {
+				if (field[x][y] == -1) continue; //there's a bomb already
+				var count = 0
+				neighbours(x, y) { tx, ty -> if (field.getOrNull(tx)?.getOrNull(ty) == -1) count++ }
+				//check if the tile should be pre-open, add the "open" flag (0b10000) in that case
+				if ((Math.abs(x - openX) <= 1) && Math.abs(y - openY) <= 1) count = count or openFlag
+				
+				field[x][y] = count
+			}
+		}
+
+		//fourth iteration: construct the game field
+		builder.apply {
+			clear()
+
+			if (w * h * 6 > 1930) {
+				appendLine("warning: too many tiles, the message will be split into multiple")
+			}
+
+			appendLine("$mines mines").appendLine()
+		
+			for (y in 0 until h) {
+				//send the current part of the game field if adding another row to it would cause an overflow
+				val nextLength = length + w * 6 
+				if (nextLength >= 1950) {
+					originalMessage!!.channel.createMessage(builder.toString())
+					clear()
+				}
+				
+				for (x in 0 until w) {
+					val value = field[x][y]
+					
+					if (value == -1) {
+						append("||").append(bombEmoji).append("||")
+					} else {
+						val emoji = numbers[value and 0b1111]
+						if (value and 0b10000 != 0) { //pre-open tile
+							append(emoji)
+						} else {
+							append("||").append(emoji).append("||")
+						}
+					}
+				}
+				append('\n')
+			}
+			if (!isEmpty()) originalMessage.channel.createMessage(toString())
+		}
+	}
+}
+
+private fun packTile(x: Int, y: Int): Int = ((x and 0xff) shl 8) or (y and 0xff);
+
+private fun unpackX(tile: Int): Int = (tile shr 8) and 0xff;
+
+private fun unpackY(tile: Int): Int = tile and 0xff;
+
+/** very straightforward */
+private inline fun neighbours(x: Int, y: Int, action: (Int, Int) -> Unit) {
+	action(x + 1, y + 1)
+	action(x    , y + 1)
+	action(x - 1, y + 1)
+	
+	action(x + 1, y    )
+	action(x - 1, y    )
+	
+	action(x + 1, y - 1)
+	action(x    , y - 1)
+	action(x - 1, y - 1)
+}
---
+++ app/src/main/kotlin/command/impl/UserinfoCommand.kt
@@ -0,0 +1,108 @@
+package flarogus.command.impl
+
+import java.io.*;
+import java.net.*;
+import java.time.format.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import kotlin.time.*;
+import kotlinx.coroutines.*;
+import dev.kord.core.behavior.channel.*;
+import dev.kord.core.entity.*;
+import dev.kord.core.event.message.*;
+import dev.kord.common.entity.*;
+import flarogus.util.*;
+import flarogus.command.*
+import flarogus.command.builder.*
+
+private val avatarFrame = ImageIO.read({}::class.java.getResource("/frame.png") ?: throw RuntimeException("avatar frame is gone"))
+private val infoFont = Font("Courier New", Font.PLAIN, 17);
+private val dateFormatter = DateTimeFormatter.ofPattern("yyyy.mm.dd HH:mm")
+private val background = Color(30, 10, 40)
+private val padding = 10;
+
+private val lines = ArrayList<String>(15)
+
+/** result:
+/------\ impostor#3661
+|avatar| impostor: yes
+\------/ ...
+*/
+@OptIn(kotlin.time.ExperimentalTime::class)
+fun addUserinfoSubcommand() = subcommand<BufferedImage?>() {
+	arguments {
+		default<Snowflake>("user", "The user whose info you want to get. Can be an id or a mention. If not present, uses the caller instead.") {
+			discordMessage?.asMessage()?.author?.id ?: Snowflake.NONE
+		}
+	}
+
+	action {
+		val user = Vars.supplier.getUser(args.arg<Snowflake>("user"))
+		
+		val userpfp = ImageIO.read(URL(user?.getAvatarUrl() ?: throw CommandException("userinfo", "could not retreive user avatar")))
+		val cropped = ImageUtil.multiply(avatarFrame, userpfp);
+		
+		lines.apply {
+			clear()
+			add((if (user.isBot) "bot" else "user") + " info")
+			add(user.tag)
+			add("impostor: " + if (user.discriminator.toInt() % 7 == 0) "yes" else "no")
+			add("user id: ${user.id}")
+			//i wasted 3 hours of my life trying to figure out how to do these two. i utterly failed. fuck instant and other stuff.
+			add("age: ${formatTime(user.id.timeMark.elapsedNow().toLong(DurationUnit.MILLISECONDS))}")
+			//add("registered at ${dateFormatter.format(user.id.timestamp as java.time.temporal.TemporalAccessor)} UTC")
+		}
+		
+		var graphics = cropped.createGraphics();
+		val metrics = graphics.getFontMetrics(infoFont);
+		
+		val perLine = metrics.height + padding;
+		var width = 0;
+		var height = 0;
+		lines.forEach {
+			var w = padding + metrics.stringWidth(it) + padding
+			if (height <= cropped.height + padding * 2) w += cropped.width + padding * 2 
+			
+			width = Math.max(width, w)
+			height += perLine
+		}
+		height = Math.max(padding * 2 + cropped.height, height)
+		graphics.dispose()
+		
+		val newImage = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+		graphics = newImage.createGraphics()
+		graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		
+		graphics.setPaint(background)
+		graphics.fillRect(0, 0, newImage.width, newImage.height)
+		graphics.drawImage(cropped, padding, padding, null)
+		
+		graphics.setPaint(Color.WHITE)
+		graphics.setFont(infoFont)
+		for (i in 0 until lines.size) {
+			var x = padding
+			val y = perLine * (i + 1)
+			if (y <= cropped.height + padding * 2) x += cropped.width + padding * 2
+			
+			graphics.drawString(lines[i], x, y)
+		}
+		
+		ByteArrayOutputStream().use {
+			ImageIO.write(newImage, "png", it);
+			ByteArrayInputStream(it.toByteArray()).use {
+				message.channel.createMessage {
+					messageReference = message.id
+					addFile("impostor.png", it)
+				}
+			}
+		}
+		
+		graphics.dispose()
+	},
+	
+	header = "userid: String?",
+	
+	description = "Displays info of the providen user. If no user id is providen, shows info of the caller"
+)
