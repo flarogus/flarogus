95f8e9c4785bc2d9bb3c0295cd488c13f004d94a
Mnemotechnician <daytimer253@gmail.com>
2022-05-16 21:13:39 +0300

    forgot a git add

---
+++ app/src/main/kotlin/server/FlarServer.kt
@@ -0,0 +1,28 @@
+package flarogus.server
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import flarogus.multiverse.*
+
+object FlarServer {
+	fun launch(port: Int) {
+		embeddedServer(Netty, port = port) {
+			install(ContentNegotiation) {
+				json()
+			}
+
+			routing {
+				get("/") {
+					Log.info { "Incoming GET request on uri: ${call.request.uri}" }
+					call.respondText("AMOGUS!")
+				}
+			}
+		}.start(wait = true)
+	}
+}
