2ff2d769e5ad95ff6d5979df2ea2cb8858640c0b
MHeMoTexHuK <daytimer253@gmail.com>
2022-05-26 02:52:45 +0300

    oops.

--- app/src/main/kotlin/command/impl/ManagementCommands.kt
+++ app/src/main/kotlin/command/impl/ManagementCommands.kt
@@ -1,6 +1,6 @@
 package flarogus.command.impl
 
-import dev.kord.common.entity.Permission
+import dev.kord.common.entity.*
 import kotlin.math.*
 import dev.kord.core.entity.*
 import dev.kord.core.entity.channel.TopGuildMessageChannel
@@ -44,19 +44,14 @@ fun TreeCommandBuilder.addManagementSubtree() = subtree("manage") {
 
 		subtree("name", "Manage the name override for this guild. The caller must be an admin of this guild.") {
 			discordOnly()
-			check { m, args ->
-				val channel = m?.channel?.asChannel() as? TopGuildMessageChannel
-				val permissions = channel?.getEffectivePermissions(m.author!!.id)
-
-				if (permissions == null) return@check "could not access your permissions."
-				if (Permission.Administrator in permissions) null else "you must have an 'admin' permission in order to use this."
-			}
 
 			subcommand<Unit>("set", "Set the name override") {
 				arguments {
 					required<String>("name", "The new name you want to assign to this guild. Up to 25 characters long.")
 				}
 				action {
+					requireAdmin()
+
 					val name = args.arg<String>("name")
 					require(name.length <= 25) { "The name cannot be longer than 25 characters! ${name.length} > 25" }
 
@@ -65,19 +60,31 @@ fun TreeCommandBuilder.addManagementSubtree() = subtree("manage") {
 			}
 
 			subaction<Unit>("reset", "Reset the name override.") {
+				requireAdmin()
 				args.arg<MultiversalGuild>("guild").nameOverride = null
 			}
 		}
 	}
 
-	presetSubtree("user") {
-		description = "Manage a multiversal user."
+	//presetSubtree("user") {
+	//	description = "Manage a multiversal user."
 
-		presetArguments {
-			default<MultiversalGuild>("guild", "The guild you want to manage. Defaults to current guild.") {
-				val id = originalMessage?.asMessage()?.data?.guildId?.value ?: fail("Anonymous caller must specify a guild")
-				Multiverse.guildOf(id) ?: fail("This guild is not valid.")
-			}
-		}
+	//	presetArguments {
+	//		default<MultiversalGuild>("guild", "The guild you want to manage. Defaults to current guild.") {
+	//			val id = originalMessage?.asMessage()?.data?.guildId?.value ?: fail("Anonymous caller must specify a guild")
+	//			Multiverse.guildOf(id) ?: fail("This guild is not valid.")
+	//		}
+	//	}
+	//}
+}
+
+private suspend fun Callback<*>.requireAdmin() {
+	require(originalMessage != null) { "anonymous caller cannot use this command" }
+	require(args.arg<MultiversalGuild>("guild").isAdmin(originalMessage().author!!.id)) {
+		"you must be an admin of this guild in order to execute this"
 	}
+}
+
+private suspend fun MultiversalGuild.isAdmin(userId: Snowflake) = channels.any {
+	(it as? TopGuildMessageChannel)?.getEffectivePermissions(userId)?.contains(Permission.Administrator) ?: false
 }
